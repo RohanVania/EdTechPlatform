fe8293841c6aa0922061d738c9daa2d21753c30e
RohanClaclaws <rohan@claclaws.com>
2023-12-27 19:04:14 -0500

    loginPage Added Css and structure left

--- FRONTEND/src/pages/LoginPage.js
+++ FRONTEND/src/pages/LoginPage.js
@@ -4,6 +4,10 @@ import React from 'react'
 function LoginPage() {
     return (
         <div id='login' className=' tw-mt-[78px] tw-bg-red-500'>
+            <div className='sm:tw-w-11/12 tw-mx-auto'>
+
+            </div>
+
         </div>
     )
 }
