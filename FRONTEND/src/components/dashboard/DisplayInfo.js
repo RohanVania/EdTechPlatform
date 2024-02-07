b2d1025732b930e8d843a634ec04c64f72f19522
RohanClaclaws <rohan@claclaws.com>
2024-02-07 17:09:17 -0500

    working on settings page ui

--- FRONTEND/src/components/dashboard/DisplayInfo.js
+++ FRONTEND/src/components/dashboard/DisplayInfo.js
@@ -11,9 +11,9 @@ function DisplayInfo() {
     return (
         <div className='tw-flex tw-flex-col tw-gap-y-4   tw-flex-wrap tw-border-[1px] tw-border-richblack-700 tw-bg-richblack-800 tw-w-full tw-p-3 md:tw-p-8 md:tw-px-12 tw-justify-between tw-items-center tw-py-8 tw-rounded-md'>
 
-            <div className='tw-flex tw-flex-wrap tw-justify-between tw-gap-y-4  tw-w-full tw-items-center'>
+            <div className='tw-flex tw-flex-wrap tw-justify-between tw-gap-y-4 tw-gap-x-3  tw-w-full tw-items-center'>
                 <h1 className='tw-capitalize md:tw-text-[15px] xl:tw-text-[19px]  tw-text-richblack-5 tw-font-semibold'>Personal Details</h1>
-                <button onClick={() => { navigate("../settings", { relative: "path" }) }} className='tw-bg-yellow-50 tw-font-semibold tw-rounded-md  tw-flex tw-items-center tw-px-6 tw-h-[40px]  dashboard-imgsetter:tw-h-[50px] tw-gap-x-2 sm:tw-text-[17px] tw-text-richblack-900 '>
+                <button onClick={() => { navigate("../settings", { relative: "path" }) }} className='tw-bg-yellow-50 tw-font-semibold tw-rounded-md  tw-flex tw-items-center tw-px-4 xs:tw-px-6 tw-h-[34px] xs:tw-h-[40px]   dashboard-imgsetter:tw-h-[50px] tw-gap-x-2 tw-text-[13px] 2xs:tw-text-[13px]  sm:tw-text-[17px]  tw-text-richblack-900 '>
                     <FaEdit />
                     Edit
                 </button>
@@ -21,18 +21,18 @@ function DisplayInfo() {
 
             <div className=' tw-w-full  tw-grid  info  tw-gap-y-4 tw-mb-[60px]  '>
                 <div className='tw-flex tw-flex-col tw-gap-y-1'>
-                    <h3 className='tw-text-richblack-100'>First Name</h3>
-                    <p className='tw-text-richblack-400'>{profileState?.firstName}</p>
+                    <h3 className='tw-text-[15x] 2xs:tw-text-[15px] sm:tw-text-[16px] xl:tw-text-[16px]  tw-text-richblack-300'>First Name</h3>
+                    <p className='tw-text-[14px]  2xs:tw-text-[15px] sm:tw-text-[16px] xl:tw-text-[16px]  tw-text-richblack-300'>{profileState?.firstName}</p>
                 </div>
                 <div className='tw-flex tw-flex-col tw-gap-y-1'>
 
-                    <h3 className='tw-text-richblack-100'>Last Name</h3>
-                    <p className='tw-text-richblack-400'>{profileState?.lastName}</p>
+                    <h3 className='tw-text-[15x] 2xs:tw-text-[15px] sm:tw-text-[16px] xl:tw-text-[16px]  tw-text-richblack-300'>Last Name</h3>
+                    <p className='tw-text-[14px]  2xs:tw-text-[15px] sm:tw-text-[16px] xl:tw-text-[16px]  tw-text-richblack-300'>{profileState?.lastName}</p>
                 </div>
                 <div className='tw-flex tw-flex-col tw-gap-y-1'>
 
-                    <h3 className='tw-text-richblack-100'>Email</h3>
-                    <p className='tw-text-richblack-400'>{profileState?.email}</p>
+                    <h3 className='tw-text-[15x] 2xs:tw-text-[15px] sm:tw-text-[16px] xl:tw-text-[16px]  tw-text-richblack-300'>Email</h3>
+                    <p className='tw-text-[14px]  2xs:tw-text-[15px] sm:tw-text-[16px] xl:tw-text-[16px]  tw-text-richblack-300'>{profileState?.email}</p>
                 </div>
                 {
                     profileState?.additionalDetails?.dateOfBirth &&
--- FRONTEND/src/components/protectedRoute/ProtectedRoute.js
+++ FRONTEND/src/components/protectedRoute/ProtectedRoute.js
@@ -5,7 +5,7 @@ import { Outlet,Navigate } from 'react-router-dom'
 
 function ProtectedRoute() {
     const {token}=useSelector((state)=>state.auth);
-    console.log("Token exist=>",token)
+    // console.log("Token exist=>",token)
     //* Here add the logic of access the global auth token from redux 
     let auth={'token':token}
 
