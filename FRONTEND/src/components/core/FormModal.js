2cdb8d24b6b7c883c423a34792bb8cddfe38076b
rohan <rohan@claclaws.com>
2024-07-27 14:35:22 -0400

    test

--- FRONTEND/src/components/core/FormModal.js
+++ FRONTEND/src/components/core/FormModal.js
@@ -6,7 +6,7 @@ import { useDispatch } from "react-redux";
 import { setLectureModal } from "../../slices/addcourseSlice";
 import { useForm } from "react-hook-form";
 
-import { Player } from 'video-react';
+import { BigPlayButton, Player } from 'video-react';
 
 
 function FormModal({ question, text, btn1, btn2, handleLogout, handleLogoutCancel }) {
@@ -74,30 +74,24 @@ function FormModal({ question, text, btn1, btn2, handleLogout, handleLogoutCance
                             <div className='tw-fex tw-flex-col'>
                                 <label className='tw-text-sm tw-text-richblack-5'>Lecture Video<sup className='tw-text-pink-200 tw-ml-1'>*</sup></label>
 
-                                <div id='filebox' className='tw-mt-5 tw-bg-richblack-700  tw-flex tw-min-h-[250px] tw-cursor-pointer tw-items-center tw-justify-center tw-rounded-md tw-border-2 tw-border-dotted tw-border-richblack-500'
+                                <div id='filebox' className='tw-mt-5 tw-bg-richblack-700  tw-flex tw-flex-col tw-min-h-[310px] tw-cursor-pointer tw-items-center tw-justify-center tw-rounded-md tw-border-2 tw-border-dotted tw-border-richblack-500'
                                     onClick={InvokeFileUpload}
-
-                                    onDragOver={handleOndragOver}
+                                    z onDragOver={handleOndragOver}
                                     onDrop={handleOnDrop}
                                 >
                                     {
 
-                                        true ?
-                                        <div className='tw-flex tw-justify-center tw-flex-col tw-items-center tw-bg-red-400  tw-flex-1 tw-h-full'>
-                                               
-                                                d
-                                                
-                                                
-                                            </div>
-                                            // <div className='tw-flex tw-flex-col tw-pb-3'>
-                                                
-                                            //     <Player aspectRatio="16:9" src={'http://localhost:3002/73bb88a9-1c22-435f-9a00-3c704b9b6685'} >
-                                            //     </Player>
-                                            //     {/* <video controls >
-                                            //         <source src='http://localhost:3002/ca055f6f-d662-4936-84ed-8330479d2fe2' />
-                                            //     </video> */}
-                                            //     <button type='button' className='tw-mt-3 tw-text-richblack-400 tw-cursor-pointer tw-underline  tw-py-1' onClick={() => { removeVideo() }}>Cancel</button>
-                                            // </div>
+                                        videoPreview ?
+
+                                            <>
+                                                <Player src={videoPreview} className="" fluid={true} aspectRatio={"16:9"}>
+                                                    <BigPlayButton className="tw-bg-red-400 " position="center">
+
+                                                    </BigPlayButton>
+                                                </Player>
+                                                <button type='button' className='tw-mt-3 tw-text-richblack-400 tw-bg-red-400  tw-cursor-pointer tw-block tw-underline  tw-py-2 tw-px-3' onClick={() => { removeVideo() }}>Cancel</button>
+                                            </>
+
                                             :
                                             <div className='tw-flex tw-justify-center tw-flex-col tw-items-center  '>
                                                 <input type='file' className='tw-hidden'
@@ -121,18 +115,31 @@ function FormModal({ question, text, btn1, btn2, handleLogout, handleLogoutCance
                                 </div>
 
                             </div>
+                            <div className='tw-flex tw-flex-col'>
+                                <label className='tw-mb-3 tw-text-sm tw-text-richblack-5'>Course Title <sup className='tw-text-pink-200 tw-ml-1'>*</sup></label>
+                                <input type='text' className=' tw-bg-richblack-700 tw-text-[#999DAA] formshadow ' placeholder='Enter Lecture Title'
+                                // {...register('courseName', {
+                                //     required: { value: true, message: 'Course name is required' },
+                                // })}
 
-                            {/* <div>
-                                 <video controls controlslist="nofullscreen nodownload noremoteplayback noplaybackrate foobar">
-                                    <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-
-                                </video> 
-                                <Player className="bg-red-900" >
-                                </Player>
-                            </div>  */}
-
-
-
+                                />
+                                {/* {
+    errors.courseName && errors.courseName.type === 'required' &&
+    <p className='tw-text-sm tw-mt-3 tw-ml-1 tw-text-pink-500'>{errors.courseName?.message}<sup className='tw-text-pink-400 tw-ml-1'>*</sup></p>
+} */}
+                            </div>
+                            <div className='tw-flex tw-flex-col'>
+                                <label className='tw-mb-3 tw-text-sm tw-text-richblack-5'>Lecture Description<sup className='tw-text-pink-200 tw-ml-1'>*</sup></label>
+                                <textarea placeholder='Enter benefits of the course' className='tw-w-full tw-min-h-[120px] formshadow tw-bg-richblack-700 tw-text-[#999DAA] tw-resize-none '
+                                // {...register('whatYouWillLearn', {
+                                //     required: { value: true, message: "Benefits of the course is required" }
+                                // })}
+                                />
+                                {/* {
+errors.whatYouWillLearn && errors.whatYouWillLearn.type === 'required' &&
+<p className='tw-text-sm tw-mt-3 tw-ml-1 tw-text-pink-500'>{errors.whatYouWillLearn?.message}<sup className='tw-text-pink-400 tw-ml-1'>*</sup></p>
+} */}
+                            </div>
 
                             <div className='tw-flex   tw-justify-end tw-gap-x-4'>
                                 <button className='tw-ml-aut  tw-flex tw-items-center tw-bg-gray-700 tw-cursor-pointer tw-gap-x-2 tw-rounded-md tw-py-2 tw-px-5 tw-font-semibold tw-text-white ' onClick={() => { cancelFormModal() }} >
@@ -148,7 +155,7 @@ function FormModal({ question, text, btn1, btn2, handleLogout, handleLogoutCance
                     </div>
                 </div>
 
-            </div>
+            </div >
         </>
     )
 }
@@ -157,29 +164,4 @@ export default FormModal;
 
 
 
-// <div className='tw-flex tw-flex-col'>
-// <label className='tw-mb-3 tw-text-sm tw-text-richblack-5'>Course Title <sup className='tw-text-pink-200 tw-ml-1'>*</sup></label>
-// <input type='text' className=' tw-bg-richblack-700 tw-text-[#999DAA] formshadow ' placeholder='Enter Lecture Title'
-// // {...register('courseName', {
-// //     required: { value: true, message: 'Course name is required' },
-// // })}
 
-// />
-// {/* {
-//     errors.courseName && errors.courseName.type === 'required' &&
-//     <p className='tw-text-sm tw-mt-3 tw-ml-1 tw-text-pink-500'>{errors.courseName?.message}<sup className='tw-text-pink-400 tw-ml-1'>*</sup></p>
-// } */}
-// </div>
-
-// <div className='tw-flex tw-flex-col'>
-// <label className='tw-mb-3 tw-text-sm tw-text-richblack-5'>Lecture Description<sup className='tw-text-pink-200 tw-ml-1'>*</sup></label>
-// <textarea placeholder='Enter benefits of the course' className='tw-w-full tw-min-h-[120px] formshadow tw-bg-richblack-700 tw-text-[#999DAA] tw-resize-none '
-// // {...register('whatYouWillLearn', {
-// //     required: { value: true, message: "Benefits of the course is required" }
-// // })}
-// />
-// {/* {
-// errors.whatYouWillLearn && errors.whatYouWillLearn.type === 'required' &&
-// <p className='tw-text-sm tw-mt-3 tw-ml-1 tw-text-pink-500'>{errors.whatYouWillLearn?.message}<sup className='tw-text-pink-400 tw-ml-1'>*</sup></p>
-// } */}
-// </div>
